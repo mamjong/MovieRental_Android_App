492668a0a0634e0a580f7e1f78fdf462d84b397e
Mark <mamjong@avans.nl>
2017-06-13 17:06:01 +0200

    Login backend in progress.

--- Prog4Tent/app/src/main/java/com/example/mark/prog4tent/LoginActivity.java
+++ Prog4Tent/app/src/main/java/com/example/mark/prog4tent/LoginActivity.java
@@ -2,12 +2,90 @@ package com.example.mark.prog4tent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailText, passwordText;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailText = (EditText) findViewById(R.id.login_et_email);
+        passwordText = (EditText) findViewById(R.id.login_et_password);
+        loginBtn = (Button) findViewById(R.id.login_btn_login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    public void volleyLogin(String un, String pw) {
+
+        final String username = un;
+        final String password = pw;
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String url = "url here...";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TEMP", "Something went wrong");
+                    }
+                }) {
+
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            public byte[] getBody() throws AuthFailureError {
+                String mContent = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
+                byte[] body = new byte[0];
+                try {
+                    body = mContent.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return body;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
