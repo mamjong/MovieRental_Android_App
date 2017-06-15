ccc51f63c20b37d213df17d65e5e2ac679ba8812
Mark <mamjong@avans.nl>
2017-06-15 15:46:05 +0200

    Movies progress.

--- Prog4Tent/app/src/main/java/com/example/mark/prog4tent/fragments/MoviesFragment.java
+++ Prog4Tent/app/src/main/java/com/example/mark/prog4tent/fragments/MoviesFragment.java
@@ -1,19 +1,40 @@
 package com.example.mark.prog4tent.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.mark.prog4tent.MainActivity;
 import com.example.mark.prog4tent.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Mark on 15-6-2017.
  */
 
 public class MoviesFragment extends Fragment {
+
+    public static final String PREFS_NAME_TOKEN = "Prefsfile";
 
     @Nullable
     @Override
@@ -26,5 +47,55 @@ public class MoviesFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Movies");
+
+    }
+
+    public void volleyMoviesRequest() {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        String url = "http://145.49.21.149:8080/api/v1/films?offset=0&count=30";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //TODO: on response
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TEMP", "Something went wrong");
+                    }
+                }) {
+
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                SharedPreferences preferences = getContext().getSharedPreferences(PREFS_NAME_TOKEN, Context.MODE_PRIVATE);
+                String token = preferences.getString("TOKEN", "No token");
+
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                headers.put("X-Access-Token", token);
+                return headers;
+            }
+
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            public byte[] getBody() throws AuthFailureError {
+                String mContent = "{}";
+                byte[] body = new byte[0];
+                try {
+                    body = mContent.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return body;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
